f254e8e7e398b768ac8b0f9b27cb374c2d7331c0
rageusen <Raf.Geusens@UGent.be>
2020-05-28 14:29:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@
 
         // add id's to h2
         $('#content-core h2').each( function(i) {
+          if(!$(this).hasAttribute("id")) {
             $(this).attr('id', 'anchor' + i );
+          }
         });
 
         // init tocbot
